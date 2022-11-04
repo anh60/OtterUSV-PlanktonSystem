58805e916cfe31245d525646ae1212b8203137b4
anh60 <andreas.holleland@outlook.com>
2022-11-04 14:45:08 +0100

    m

--- main.py
+++ main.py
@@ -5,20 +5,14 @@ from time import sleep
 
 kit = MotorKit(i2c=board.I2C())
 
-LENS_POS = 0        # Position of lens in millimeters relative to the IBIDI slide
+LENS_POS = 100        # Position of lens in millimeters relative to the IBIDI slide
 PUMP_RATE = 0.22    # ml/sec
 
 def getLensPosition():
-    try:
-        f = open('position.txt', 'r')
-        LENS_POS = f.readline()
-        f.close()
-        print("File read")
-    except:
-        f = open('position.txt', 'w')
-        f.write(LENS_POS)
-        f.close()
-        print("file not found, creating position.txt")
+    f = open('position.txt', 'r')
+    LENS_POS = f.readline()
+    f.close()
+    print("File read", LENS_POS)
 
 def focus(mm: int):
     if mm < 0:
@@ -30,6 +24,7 @@ def focus(mm: int):
         kit.stepper1.onestep(direction=direction)
         #sleep(0.003)
 
+print(LENS_POS)
 getLensPosition()
 
 if __name__ == '__main__':
