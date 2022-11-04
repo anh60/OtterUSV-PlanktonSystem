eabbfe690b0708270cad207724716be874f5a42a
anh60 <andreas.holleland@outlook.com>
2022-11-04 15:36:28 +0100

    m

--- main.py
+++ main.py
@@ -5,27 +5,53 @@ from time import sleep
 
 kit = MotorKit(i2c=board.I2C())
 
-LENS_POS = 100        # Position of lens in millimeters relative to the IBIDI slide
+# Position of lens relative to the IBIDI slide (millimeters)
+LENS_POS = 0   
+# Minimum allowed position (Ibidi slide side)   
+LENS_POS_MIN = 0
+# Maximum allowed position (Stepper motor side)
+LENS_POS_MAX = 10000
+
 PUMP_RATE = 0.22    # ml/sec
 
-def getLensPosition():
+# Read and set lens position from 'position.txt'
+def readLensPosition():
     f = open('position.txt', 'r')
     LENS_POS = f.readline()
     f.close()
     print("File read", LENS_POS)
 
+# Write new lens position to 'position.txt'
+def writeLensPosition():
+    f = open('position.txt', 'w')
+    f.write(LENS_POS)
+    f.close()
+
+# Adjust lens position with stepper motor
 def focus(mm: int):
+    # Set direction
     if mm < 0:
         direction = stepper.BACKWARD
         mm *= -1
     else:
         direction = stepper.FORWARD
-    for _ in range(mm):
-        kit.stepper1.onestep(direction=direction)
-        #sleep(0.003)
 
-print(LENS_POS)
-getLensPosition()
+    # Move lens
+    for i in range(mm):
+        # Checking if lens is at min or max position
+        if LENS_POS == LENS_POS_MIN or LENS_POS == LENS_POS_MAX:
+            print("Position limit reached")
+            break
+        else:
+            kit.stepper1.onestep(direction=direction)
+            LENS_POS = LENS_POS + i
+            print("Lens position: ", LENS_POS)
+            #sleep(0.003)
+
+readLensPosition()
+
+focus(-1000)
+
 
 if __name__ == '__main__':
     pass
