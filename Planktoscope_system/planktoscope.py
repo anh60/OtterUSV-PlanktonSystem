87b20ed8e9fb94f1c5a062691697d2d8a1f95def
anh60 <andreas.holleland@outlook.com>
2022-12-03 19:05:31 +0100

    main

--- Planktoscope_system/planktoscope.py
+++ Planktoscope_system/planktoscope.py
@@ -6,4 +6,5 @@ from picamera import PiCamera
 camera = PiCamera()
 
 def takePicture():
-    camera.capture('/pictures/image.jpg')
+    path = '/home/pi/planktoPython/pictures/OtterUSV-PlanktonSystem/Planktoscope_system/pictures/image.jpg'
+    camera.capture(path)
