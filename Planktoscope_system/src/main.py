6909c10ff668f6167f4887123fce661573ba8ea8
anh60 <andreas.holleland@outlook.com>
2022-12-19 21:02:38 +0100

    fix

--- Planktoscope_system/src/main.py
+++ Planktoscope_system/src/main.py
@@ -3,19 +3,15 @@
 # 2022
 #
 
-import lens_focus as lens
 import rms_com as rms
-import planktoscope as ps
 
 i = 1
 while(i == 1):
     sel = input('WHAT IS? [1=FOCUS, 2=RMS, 3=CAMERA]\n')
     if(sel == '1'):
-        lens.adjustLens()
     elif (sel == '2'):
         rms.sendCMD()
     elif (sel == '3'):
-        ps.takePicture()
     else:
         print('Invalid input')
 
