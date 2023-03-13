de6ea53cf0529ce4239fdf46da622a7a8f6c1ebf
anh60 <andreas.holleland@outlook.com>
2023-03-13 16:04:32 +0100

    stepper pump test

--- Planktoscope_system/src/stepper_pump.py
+++ Planktoscope_system/src/stepper_pump.py
@@ -5,13 +5,12 @@
 #
 
 import board
+import time
 from adafruit_motorkit import MotorKit
 from adafruit_motor import stepper
 
 kit = MotorKit(i2c=board.I2C())
 
-direction = stepper.BACKWARD
-style = stepper.SINGLE
-
-while True:
-    kit.stepper1.onestep(direction=direction, style=style)
+for i in range(100):
+    kit.stepper1.onestep()
+    time.sleep(0.01)
