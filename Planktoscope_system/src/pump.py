8b9c33a757e8fcb832412f31cfe7046fc447cfdd
anh60 <andreas.holleland@outlook.com>
2023-03-14 15:39:01 +0100

    dc-motor-pump

--- Planktoscope_system/src/pump.py
+++ Planktoscope_system/src/pump.py
@@ -3,12 +3,16 @@
 # 2022
 #
 
+import time
 import board
 from adafruit_motorkit import MotorKit
 
-kit = MotorKit(i2c=board.I2C())
+kit = MotorKit(i2c=board.I2C()) 
 
-PUMP_RATE = 0.22    # ml/sec
+# Pump rate = 100mL/min at throttle=1.0
+#           = 1.6mL/sec at throttle=1.0
+
+PUMP_RATE = 0.22    # mL/sec     
 
 def pump(ml: int):
     if ml < 0:
@@ -16,5 +20,7 @@ def pump(ml: int):
         ml *= -1
     else:
         kit.motor3.throttle = 0.5
-    sleep(ml / PUMP_RATE)
+    time.sleep(ml / PUMP_RATE)
     kit.motor3.throttle = None
+
+kit.motor3.throttle = 1.0
