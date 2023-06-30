3ac7e5abb40340ad735b295c314d0b98a821e6fa
anh60 <andreas.holleland@outlook.com>
2023-06-30 15:26:58 +0200

    mqtt c++ test, database test, changed to uart pins on rpi

--- Planktoscope_system/src/RMS_COM/rms_com.py
+++ Planktoscope_system/src/RMS_COM/rms_com.py
@@ -14,9 +14,15 @@ import STATUS.status as state
 
 #---------------------------- GLOBALS ------------------------------------------
 
+# UART TX/RX
+port = '/dev/ttyS0'
+
 # Bottom USB3.0 (blue)
-port = '/dev/ttyUSB0'
+#port = '/dev/ttyUSB0'
+
+# Laptop USB port
 #port = 'COM4'
+
 baud = 9600
 
 C_PUMP      = 0x01
