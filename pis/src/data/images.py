4a82170fe01f7a9665ff96dd27e547c675777fdf
anh60 <andreas.holleland@outlook.com>
2023-07-14 17:54:41 +0200

    test 3

--- pis/src/data/images.py
+++ pis/src/data/images.py
@@ -29,9 +29,7 @@ class column(str, Enum):
 # Get date/time of all samples taken (sample_times)
 def get_sample_times():
     sample_names = [f.name for f in os.scandir(samples_path) if f.is_dir()]
-
-    for n in sample_names:
-        print(sample_names[n], '\n')
+    print(sample_names)
         
             
 
