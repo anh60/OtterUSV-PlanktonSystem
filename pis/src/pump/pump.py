d06e00f1adbf42234bf6530c94b9968e8e8d917d
anh60 <andreas.holleland@outlook.com>
2023-07-18 18:28:50 +0200

    fix

--- pis/src/pump/pump.py
+++ pis/src/pump/pump.py
@@ -17,7 +17,7 @@ kit = MotorKit(i2c=board.I2C())
 
 def pump_thread_cb():
     while True:
-        if((state.get_sys_state() >> state.status_flag.SAMPLING) & 1):
+        if((state.get_sys_state() >> state.status_flag.PUMP) & 1):
             kit.motor3.throttle = -1.0
         else:
             kit.motor3.throttle = None
