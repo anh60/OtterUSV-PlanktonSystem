0cb9cd9fa549fd15fbcfdaa2779995c3228e1261
anh60 <andreas.holleland@outlook.com>
2023-09-10 19:24:13 +0200

    trying to fix pos on PIS

--- pis/src/sample/sample.py
+++ pis/src/sample/sample.py
@@ -64,7 +64,9 @@ def set_sample_pos(msg):
         print('setting pos')
         lat = round(float(coordinates[0]), 5)
         lon = round(float(coordinates[1]), 5)
+        print('it worked')
     except:
+        print('didnt work')
         lat = 63.41729
         lon = 10.40609
 
